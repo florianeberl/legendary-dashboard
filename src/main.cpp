809fa70f9b1f92f2cfe51c7e9d85ccdf574d7697
stefanreisinger <66374845+stefanreisinger@users.noreply.github.com>
2020-06-03 21:56:32 +0200

    First implememtation of SIM800L functions

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,29 @@
 
 Adafruit_BME280 bme; // I2C
 
+// TTGO T-Call pin definitions
+#define MODEM_RST            5
+#define MODEM_PWKEY          4
+#define MODEM_POWER_ON       23
+#define MODEM_TX             27
+#define MODEM_RX             26
+#define I2C_SDA              21  // Original example sets "KeepOn Mode" via I2C in IP5306, looks like only needed when powered from battery?
+#define I2C_SCL              22  // curretly not implemented
+
+// Configure TinyGSM library
+#define TINY_GSM_MODEM_SIM800      // Modem is SIM800
+#define TINY_GSM_RX_BUFFER   1024  // Set RX buffer to 1Kb
+#include <TinyGsmClient.h>
+
+// SIM800L includes, defines and variables
+#define SerialAT  Serial1
+#define SMS_TARGET  "+491719552455"
+const char simPIN[]   = "1234"; // SIM card PIN code, if any
+
+TinyGsm modem(SerialAT);
+
+
+
 #include <statistics.h>
 Statistics tempStats;
 Statistics humStats;
@@ -21,11 +44,43 @@ float currentHumidityPercent = 0;
 float currentPressurePascal = 0;
 int frameCounter = 0;
 
+
+
+
+// helper function to send AT Command to modem and write command and result to Monitor
+String sendATcommand(const char *toSend, unsigned long milliseconds)
+{
+  String result;
+
+  Serial.print("Sending AT command: ");
+  Serial.println(toSend);
+  SerialAT.println(toSend);
+
+  unsigned long startTime = millis();
+  Serial.print("Received AT result: ");
+
+  while (millis() - startTime < milliseconds) {
+    if (SerialAT.available()) {
+      char c = SerialAT.read();
+      Serial.write(c);
+      result += c;  // append to the result string
+    }
+  }
+
+Serial.println();  // new line after timeout.
+
+return result;
+}
+
+
+
 // run once on startup
 void setup()
 {
   // Setup serial connection for debugging
   Serial.begin(115200);
+
+  Serial.println("--------------- Startup -----------------");
 
   // Initialize Environment Sensor
   if (!bme.begin(BME280_ADDR, &Wire))
@@ -42,6 +97,60 @@ void setup()
                   Adafruit_BME280::SAMPLING_X16,    /* Pressure oversampling */
                   Adafruit_BME280::FILTER_X16,      /* Filtering. */
                   Adafruit_BME280::STANDBY_MS_500); /* Standby time. */
+
+
+  // Initialize SIM800L Module
+  // Set-up modem reset, enable, power pins
+  pinMode(MODEM_PWKEY, OUTPUT);
+  pinMode(MODEM_RST, OUTPUT);
+  pinMode(MODEM_POWER_ON, OUTPUT);
+  digitalWrite(MODEM_PWKEY, LOW);
+  digitalWrite(MODEM_RST, HIGH);
+  digitalWrite(MODEM_POWER_ON, HIGH);
+
+  // Set GSM module baud rate and UART pins
+  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  delay(3000);
+
+  // Restart takes quite some time
+  // use modem.init() if you don't need the complete restart
+  Serial.println("Initializing modem...");
+  modem.restart();
+  
+  String name = modem.getModemName();
+  Serial.print("Modem Name: ");
+  Serial.println(name);
+
+  String modemInfo = modem.getModemInfo();
+  Serial.print("Modem Info: ");
+  Serial.println(modemInfo);
+
+  // Unlock your SIM card with a PIN if needed
+  if (strlen(simPIN) && modem.getSimStatus() != 3 ) {
+    Serial.println("Unlocking SIM...");
+    modem.simUnlock(simPIN);
+  }
+
+  Serial.print("Waiting for network...");
+  if (!modem.waitForNetwork(240000L)) {
+    Serial.println(" fail");
+    Serial.println("[ ERROR ] - Could not connect to mobile network, check antenna!");
+    while (1);
+  }
+  Serial.println(" OK");
+
+  // maybe not really needed, since we just connected to the network, but maybe useful in the loop part
+  if (modem.isNetworkConnected()) {
+    Serial.println("Network connected!");
+  }
+
+  int csq = modem.getSignalQuality();
+  Serial.print("Sigal Quality [0-31]: ");
+  Serial.println(csq);
+
+  // Network time is not set properly if requested too early after network connection
+  delay(4000);
+
 }
 
 // run forever
@@ -87,5 +196,66 @@ void loop()
   Serial.print(bme.readAltitude(SEALEVEL_PRESSURE + SEALEVEL_PRESSURE_CALIBRATION));
   Serial.print(" m");
 
+
+  //
+  // Get network time as a string
+  String time = modem.getGSMDateTime(DATE_FULL);
+  Serial.print("Current Network Time [String]: ");
+  Serial.println(time);
+
+
+ // Get network time in individual values
+  int year = 0;
+  int month = 0;
+  int day = 0;
+  int hour = 0;
+  int min = 0;
+  int sec = 0;
+  float timezone = 0;
+
+  if (modem.getNetworkTime(&year, &month, &day, &hour, &min, &sec,
+                                 &timezone)) {
+    Serial.println("Current Network Time (Values): ");
+    Serial.print("Year: ");
+    Serial.print(year);
+    Serial.print("  - Month: ");
+    Serial.print(month);
+    Serial.print("  - Day: ");
+    Serial.print(day);
+    Serial.print("  - Hour: ");
+    Serial.print(hour);
+    Serial.print("  - Minute: ");
+    Serial.print(min);
+    Serial.print("  - Second: ");
+    Serial.print(sec);
+    Serial.print("  - Timezone: ");
+    Serial.println(timezone);
+  }
+  else
+    Serial.println("[ERROR] Couldn't get network time.");
+ 
+  // Send a SMS message
+//  bool res = modem.sendSMS(SMS_TARGET, String("Hier ist das ESP32 Board!"));
+//  if (res) { 
+//    Serial.print("SMS sent to ");
+//    Serial.println(SMS_TARGET);
+//  }
+//  else
+//    Serial.println("[ERROR] SMS failed!");
+
+
+  // set SMS text format
+  sendATcommand("AT+CMGF=1" , 2000);
+  // request unread received SMS mesages
+//  sendATcommand("AT+CMGL=\"REC UNREAD\"" , 2000);
+  // request all SMS mesages
+  sendATcommand("AT+CMGL=\"ALL\"" , 2000);
+
+  // Delete all SMS messages except for unread ones
+//  sendATcommand("AT+CMGD=1,3" , 2000);
+
   delay(2000);
 }
+
+
+
